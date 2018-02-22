a87f5e0b7cc85b469a0d6e9e7e3b28b7169080ad
stoyanovdmitry <stoyanov.dmt@gmail.com>
2018-02-22 16:45:47 +0200

    data module User (test)

---
+++ data/src/main/java/com/todo/entity/User.java
@@ -0,0 +1,38 @@
+package com.todo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue
+	private int id;
+
+	private String username;
+
+	public User() {
+	}
+
+	public User(String username) {
+		this.username = username;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+}
