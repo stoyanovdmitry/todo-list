09cc144e4c5ca9fbbb94f43ae047b4c781af7e32
Dmitry Stoyanov <stoyanov.dmt@gmail.com>
2018-05-03 16:26:27 +0300

    User password write only by Jackson

--- data/src/main/java/com/todo/entity/User.java
+++ data/src/main/java/com/todo/entity/User.java
@@ -1,7 +1,5 @@
 package com.todo.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import javax.persistence.*;
@@ -18,6 +16,7 @@ public class User {
 	private String username;
 
 	@Column(nullable = false)
+	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
 	private String password;
 
 	@OneToMany(mappedBy = "user")
