facbd76cf6cdde18b81cf2479e58348390e8b370
Dmitry Stoyanov <stoyanov.dmt@gmail.com>
2018-05-09 15:19:43 +0300

    cleared from imports

--- rest-api/src/main/java/com/todo/RestApplication.java
+++ rest-api/src/main/java/com/todo/RestApplication.java
@@ -1,16 +1,8 @@
 package com.todo;
 
-import com.todo.entity.Todo;
-import com.todo.entity.User;
-import com.todo.repository.TodoRepository;
-import com.todo.repository.UserRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 @EnableJpaRepositories
