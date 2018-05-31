4a5c9d96ebf732ce3a960a3c98d6e68eac3143bf
Dmitry Stoyanov <stoyanov.dmt@gmail.com>
2018-05-31 16:19:22 +0300

    added remote Postgres DB

--- rest-api/src/main/java/com/todo/security/jwt/JwtConstants.java
+++ rest-api/src/main/java/com/todo/security/jwt/JwtConstants.java
@@ -7,10 +7,10 @@ public class JwtConstants {
 	public static final String JWT_PREFIX = "Bearer ";
 
 	public static final String ACCESS_SECRET = "secret";
-	public static final long ACCESS_EXPIRATION = 10_000; // 864_000_000 = 10 days
+	public static final long ACCESS_EXPIRATION = 864_000_000; // 864_000_000 = 10 days
 	public static final String ACCESS_HEADER = "Access-Token";
 
 	public static final String REFRESH_SECRET = "refresh";
-	public static final long REFRESH_EXPIRATION = 60_000;
+	public static final long REFRESH_EXPIRATION = 2_592_000_000L;	// 2_592_000_000L = 30 days
 	public static final String REFRESH_HEADER = "Refresh-Token";
 }
