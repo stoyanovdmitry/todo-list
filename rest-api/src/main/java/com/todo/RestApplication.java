95037b6559d0b234049899f489db56677a77845d
Dmitry Stoyanov <stoyanov.dmt@gmail.com>
2018-06-05 14:04:21 +0300

    removed '/' before build.js path and added ResourceHandlerRegistry

--- rest-api/src/main/java/com/todo/RestApplication.java
+++ rest-api/src/main/java/com/todo/RestApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -25,6 +26,22 @@ public class RestApplication {
 						.allowedOrigins("http://localhost:8081")
 						.allowedMethods("POST", "GET", "PUT", "DELETE")
 						.exposedHeaders("Access-Token", "Refresh-Token");
+			}
+
+			@Override
+			public void addResourceHandlers(ResourceHandlerRegistry registry) {
+				final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+						"classpath:/META-INF/resources/", "classpath:/resources/",
+						"classpath:/static/", "classpath:/public/" };
+
+				if (!registry.hasMappingForPattern("/webjars/**")) {
+					registry.addResourceHandler("/webjars/**").addResourceLocations(
+							"classpath:/META-INF/resources/webjars/");
+				}
+				if (!registry.hasMappingForPattern("/**")) {
+					registry.addResourceHandler("/**").addResourceLocations(
+							CLASSPATH_RESOURCE_LOCATIONS);
+				}
 			}
 		};
 	}
