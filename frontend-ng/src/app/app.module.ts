e5c7769f7c04b792a2d9653688f51df392851c85
Dmitry Stoyanov <stoyanov.dmt@gmail.com>
2018-07-31 14:45:16 +0300

    added routing with auth init

--- frontend-ng/src/app/app.module.ts
+++ frontend-ng/src/app/app.module.ts
@@ -2,19 +2,28 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
+import {RouterModule, Routes} from '@angular/router';
 
 import {AppComponent} from './app.component';
 import {TodosComponent} from './todos/todos.component';
+import {AuthorizationComponent} from './authorization/authorization.component';
+
+const routes: Routes = [
+  {path: '', component: TodosComponent},
+  {path: 'login', component: AuthorizationComponent},
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodosComponent
+    TodosComponent,
+    AuthorizationComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
